0b189fee7e3d2712ddde2ca9924cb4c01a3da6af
ravipati-madhulika <ravipatimadhulika205@gmail.com>
2026-03-06 23:34:57 +0530

    fix commit email

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,6 @@ import Hero from "@/components/Hero";
 export default function Home(): JSX.Element {
   return (
     <main style={{ position: "relative", minHeight: "100vh" }}>
-      <DottedBackground />
-      <Hero />
     </main>
   );
 }
