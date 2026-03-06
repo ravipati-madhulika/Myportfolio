c5ea547961f5ff554b0f71b346330bfe041d8aa8
ravipati-madhulika <ravipatimadhulika205@gmail.com>
2026-03-06 23:35:22 +0530

    dottedbg

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import Hero from "@/components/Hero";
 export default function Home(): JSX.Element {
   return (
     <main style={{ position: "relative", minHeight: "100vh" }}>
+      <DottedBackground />
+      <Hero />
     </main>
   );
 }
