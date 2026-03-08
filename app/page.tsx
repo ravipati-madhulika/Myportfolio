8405eb1f4cab771f97c03b597ecc96192d65972a
ravipati-madhulika <ravipatimadhulika205@gmail.com>
2026-03-08 13:19:20 +0530

    portfolio

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import DottedBackground from "@/components/DottedBackground";
+import Hero from "@/components/Hero";
+
+export default function Home(): JSX.Element {
+  return (
+    <main style={{ position: "relative", minHeight: "100vh" }}>
+      <DottedBackground />
+      <Hero />
+    </main>
+  );
+}
