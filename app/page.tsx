e56aef60a736e6842d81d22d3ac2f19a980fb1d9
ravipati-madhulika <ravipatimadhulika205@gmail.com>
2026-03-21 21:13:08 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -9,14 +9,3 @@ export default function Home(): JSX.Element {
     </main>
   );
 }
-import DottedBackground from "@/components/DottedBackground";
-import Hero from "@/components/Hero";
-
-export default function Home(): JSX.Element {
-  return (
-    <main style={{ position: "relative", minHeight: "100vh" }}>
-      <DottedBackground />
-      <Hero />
-    </main>
-  );
-}
